07c78bc78312bf83ad4e312ea5faebafdedb6b14
NikitaDubrovskii <inikita.dubrovskii@gmail.com>
2024-08-08 01:20:43 +0300

    Включено расписание

--- back/tgbot/starter/src/main/java/dev/dubrovsky/starter/TgbotApplication.java
+++ back/tgbot/starter/src/main/java/dev/dubrovsky/starter/TgbotApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "dev.dubrovsky")
 @EnableJpaRepositories(basePackages = "dev.dubrovsky.repository")
 @EntityScan(basePackages = "dev.dubrovsky.model")
+@EnableScheduling
 public class TgbotApplication {
 
     public static void main(String[] args) {
